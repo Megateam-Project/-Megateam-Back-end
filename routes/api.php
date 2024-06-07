5267f793811fa21457ed421698d4a9e542c6d57d
Tan <tan.a25@student.passerellesnumeriques.org>
2024-06-07 22:54:15 +0700

    update profile user

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,6 @@ Route::namespace('api')->group(function(){
     Route::post('login', [AuthController::class,'login']);
     Route::post('signup', [AuthController::class,'signup']);
     Route::post('logout', [AuthController::class,'logout']);
-    Route::post('profile', [AuthController::class,'profile']);
+    Route::get('profile', [AuthController::class,'profile']);
 });
 
